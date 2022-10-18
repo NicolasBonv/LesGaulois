3aca5cf8e48ba8bc134312c901cf1f3c9e398fb6
bonvalet nicolas <bnn3786a@ens.fsi>
2022-10-18 17:38:50 +0200

    Enumérés tp 2 manque tableaux deux cases + finir tp2 et tp3 chez nous

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,8 +1,10 @@
 package personnages;
-
+import Equipement;
 public class Romain{
 	private String nom;
 	private int force;
+	private int nbEquipement = 0;
+	private Equipement equipement[][];
 	public Romain(String nom,int force) {
 		this.nom = nom;
 		this.force = force;
@@ -31,12 +33,25 @@ public class Romain{
 	public String toString() {
 		return "Romain [nom=" + nom + ", force=" + force + "]";
 	}
+	public void sEquiper(Equipement equipement) {
+		switch(nbEquipement) {
+		case 2 : System.out.println("Le soldat " + nom + " est deja bien protégé !");
+		case 1 : if (equipement[0][0] == equipement) {
+			System.out.println("Le soldat " + nom + " possede déjà " + equipement.getNom() + " !");
+		}
+		default : nbEquipement += 1;
+				equipement[Equipement.CASQUE] = equipement;
+				System.out.println("Le soldat "  + nom +" s'équipe avec un " + equipement.getNom());
+		}
+	}
 	public static void main(String[] args) {
 		Romain minus = new Romain("Minus",6);
-		System.out.println(minus);
+		Equipement equipement = Equipement.CASQUE;
+		/*System.out.println(minus);
 		System.out.println(minus.prendreParole()); 
 		minus.parler("coucou je suis un romain");
-		minus.recevoirCoup(5);
+		minus.recevoirCoup(5);*/
 		//TODO crï¿½er un main permettant de tester la classe Romain
+		minus.sEquiper(equipement);
 }
 }
